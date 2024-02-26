8259d5001a73079e2d060f739908b3b99fa4abdc
Jackson Jones <jacksonpjones05@gmail.com>
2024-02-26 16:33:39 -0700

    edit and try to make it work again

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const apikey = 'https://random-word-api.vercel.app/api?words=5'
 
 let user = {
-    name : "Gabe Pettingill",
-    email : "gaberielbabriel@sauce.com",
-    pass: "restricted",
+    name : "",
+    email : "",
+    pass: "",
     stories : []
 }
 function Story(title, genre, content) {
@@ -55,7 +55,7 @@ function check_login(name, email, pass) {
     passy = document.getElementById(pass).value;
 
     update_content();
-
+    try {
     if (user.name === namey && user.pass == passy) {
         const newAlert = document.createElement('div')
         newAlert.style.alignSelf = 'center';
@@ -84,6 +84,16 @@ function check_login(name, email, pass) {
         return false;
     }
 }
+catch (err) {
+    const newAlert = document.createElement('div')
+        newAlert.style.alignSelf = 'center';
+        newAlert.innerHTML = "<p class='alert alert-danger'>Login failed, create an account</p>"
+        const parent = document.getElementById('login')
+        parent.appendChild(newAlert);
+        setTimeout(() => newAlert.style.display = "none", 3000)
+        return false;
+}
+}
 
 function save_login(mailbox, namebox, passbox) {
     email_container = document.getElementById(mailbox)
@@ -95,7 +105,6 @@ function save_login(mailbox, namebox, passbox) {
     user.pass = password_container.value
 
     localStorage.setItem('user', JSON.stringify(user));
-
 
     const newAlert = document.createElement('div')
         newAlert.style.alignSelf = 'center';
@@ -113,10 +122,10 @@ function update_content() {
     user = JSON.parse(localStorage.getItem('user'))
 
     for (i = 0; i < name_elements.length; i++) {
-        name_elements[i].innerText = user.name
+        name_elements[i].innerText = `--> ${user.name} <--`
     }
     for (i = 0; i < mail_elements.length; i++) {
-        mail_elements[i].innerText = user.email
+        mail_elements[i].innerText = `Email: ${user.email}`
     }
 } 
 
@@ -125,16 +134,14 @@ function update_content() {
 
 function generate_story(title, genre) {
 
-    update_content();
+    update_content()
 
-    let current = new Story(document.getElementById(title).value, document.getElementById(genre).value);
+    let current = new Story(document.getElementById(title).value, document.getElementById(genre).value)
     user.stories.push(current)
-    
+    console.log(JSON.stringify(user))
     localStorage.setItem('user', JSON.stringify(user))
 
     window.location.href = 'write.html'
-
-    retrieve_story(user.stories.length - 1)
 
 }
 function retrieve_story(story_loc) {
@@ -178,9 +185,19 @@ function gen_story_list(list) {
         curr_item.innerHTML = `<em>${item.title}</em> (${item.genre}). <button class="btn" onclick="retrieve_story(${count})">Write?</button><button onclick="dlt(${count})" class="btn btn-danger">Delete?</button>`
         count++
     })
-    if(stories.length === 1) {
-        p = doucment.createElement('p')
+    if(stories.length === 0) {
+        console.log('trippepd just don"t work')
+        p = document.createElement('p')
+        p.classList.add('alert')
+        p.classList.add('alert-secondary')
+        p.style.width = "50%"
+        let empty = document.getElementById('empty');
+        empty.style.display = 'flex'
+        empty.style.flexDirection = 'column'
+        empty.style.justifyContent = 'center'
+        p.style.alignSelf = "center"
         document.getElementById('empty').appendChild(p)
+
         p.innerHTML = `Don't see anything here? <a href='create.html'>Create</a> a story!`
     }
 
