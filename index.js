2727dfcc111df064a0be9a0d8b921d38f299deca
Jackson Jones <jacksonpjones05@gmail.com>
2024-03-04 17:12:16 -0700

    let's fix things

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+
+const app = express();
 const port = process.env.PORT || 8080;
-const app = express()
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname, './index.html')
-})
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
-app.listen(port, () => {
-    console.log('listening on port 3000')
-})
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
