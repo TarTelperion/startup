dcb13e6306f5e1b2ba25f31ae6a5d2759a9d0906
Jackson Jones <jacksonpjones05@gmail.com>
2024-03-06 10:45:41 -0700

    endpoint for story retrieval

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = process.argv.length > 2 ? process.argv[2] : 3000
+app.use(express.static('public'))
+
+apiRoute = express.Router()
+app.use('/api', apiRoute)
+
+let stories = []
+
+// Retrieve story by ID and send by JSON stringify
+apiRoute.get('/stories/:id', (req, res) => {
+    let helpful = false
+    stories.forEach((story) => {
+        if (story.id === id) {
+            res.send(JSON.stringify(story))
+            helpful = true
+        }
+    })
+    if (!helpful) {
+        res.send({error : "no story with that ID found"})
+    }
+})
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
