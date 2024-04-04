88d76ad73effca2ebeeb2c1689b7bd79395f9732
Jackson Jones <jacksonpjones05@gmail.com>
2024-04-03 21:19:53 -0600

    add vite config to main branch

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        app: './src/index.html',
+      },
+    },
+  },
+})
