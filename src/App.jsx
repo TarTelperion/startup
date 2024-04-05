f76bd3101ffaa6ede4301480695d4068fb8e1d53
Jackson Jones <jacksonpjones05@gmail.com>
2024-04-05 09:07:16 -0600

    add material theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Root from './Root'
 import MiniDrawer from './AppFrame'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import '@fontsource/spectral'
-import './styles.css'
 
-createRoot(document.getElementById('root')).render(<MiniDrawer />)
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#387780',
+    },
+    secondary: {
+      main: '#FA8072',
+    },
+    success: {
+      main: '#cad2c5',
+    },
+    error: {
+      main: '#f44336',
+    },
+    info: {
+      main: '#dbd4d3',
+    },
+  },
+  typography: {
+    fontFamily: '"Spectral", "Helvetica", "Arial", sans-serif',
+  },
+})
+
+createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <MiniDrawer />
+  </ThemeProvider>
+)
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import Container from '@mui/material/Container'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-
-const theme = createTheme()
 
 const App = () => {
   return (
