55de2588965393188a1a74815b043baa3a92218c
Jackson Jones <jacksonpjones05@gmail.com>
2024-04-08 18:12:03 -0600

    joined call

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -10,6 +10,7 @@ import {
 import { useEffect } from 'react'
 import CreateIcon from '@mui/icons-material/Create'
 import { Typography, Box } from '@mui/material'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
 // Absolute Dependencies
 
 // Relative Dependencies
@@ -17,10 +18,13 @@ import { get } from '../../fetch/get'
 import { useUser } from '../../hooks/useUser'
 import { Flex, ViewHeader } from '../../layout'
 import { useJoinedStories } from '../../hooks/useJoinedStories'
+import { useGlobalStories } from '../../hooks/useGlobalStories'
+import { Delete } from '@mui/icons-material'
 
 const Home = () => {
   const { user } = useUser()
   const { stories } = useJoinedStories()
+  const { globalStories } = useGlobalStories()
 
   const paperStyles = {
     display: 'flex',
@@ -85,12 +89,53 @@ const Home = () => {
         <Flex flexColumn>
           <Flex flexColumn mb={2}>
             <Paper sx={paperStyles} elevation={4}>
-              Paper2
+              <Typography variant="subtitle2" align="center">
+                Owned Stories
+              </Typography>
+              <List>
+                {globalStories.map(
+                  ({ title, owner, authors, genre }, index) => {
+                    if (owner == user.name) {
+                      return (
+                        <ListItem
+                          key={index}
+                          secondaryAction={
+                            <ListItemIcon>
+                              <IconButton>
+                                <DeleteForeverIcon />
+                              </IconButton>
+                            </ListItemIcon>
+                          }
+                        >
+                          <ListItemText
+                            primary={title}
+                            secondary={
+                              <>
+                                <Typography
+                                  sx={{ display: 'inline' }}
+                                  component="span"
+                                  variant="body2"
+                                >
+                                  {`${genre}--`}
+                                </Typography>
+                                {authors +
+                                  `${authors > 1 ? ' authors' : ' author'}`}
+                              </>
+                            }
+                          />
+                        </ListItem>
+                      )
+                    }
+                  }
+                )}
+              </List>
             </Paper>
           </Flex>
           <Flex flexColumn>
             <Paper sx={paperStyles} elevation={4}>
-              Paper3
+              <Typography variant="subtitle2" align="center">
+                Most Recent
+              </Typography>
             </Paper>
           </Flex>
         </Flex>
