a2af2453e32aec5ffc7c2ce4f514843ee364c85f
Jackson Jones <jacksonpjones05@gmail.com>
2024-04-09 13:21:02 -0600

    edgypaper

--- src/layout/EdgyPaper.jsx
+++ src/layout/EdgyPaper.jsx
@@ -1,1 +1,29 @@
-const EdgyPaper = ({ children }) => {}
+import { Paper, Slide } from '@mui/material'
+
+const EdgyPaper = ({
+  children,
+  open = true,
+  transition = false,
+  elevation = 20,
+}) => {
+  if (transition) {
+    return (
+      <Slide
+        direction="up"
+        in={open}
+        sx={{ bottom: '-50px' }}
+        mountOnEnter
+        unmountOnExit
+      >
+        <Paper elevation={elevation}>{children}</Paper>
+      </Slide>
+    )
+  }
+  return (
+    <Paper sx={{ bottom: '-50px' }} elevation={elevation}>
+      {children}
+    </Paper>
+  )
+}
+
+export default EdgyPaper
