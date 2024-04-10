bdabc2d30034bbbfa38f4b22f644ec5467f742bb
pvjones@users.noreply.github.com <24881961+pvjones@users.noreply.github.com>
2024-04-10 10:35:34 -0600

    Updating sidebar styles

--- src/theme/components.js
+++ src/theme/components.js
@@ -1,3 +1,5 @@
+import { alpha } from '@mui/material/styles'
+
 export const components = {
   MuiButtonBase: {
     styleOverrides: {
@@ -14,6 +16,40 @@ export const components = {
           ownerState.color === 'secondary' && {
             color: '#fff',
           }),
+      }),
+    },
+  },
+  MuiListItemButton: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        color: theme.palette.primary.dark,
+        '&:hover': {
+          backgroundColor: alpha(theme.palette.primary.main, 0.2),
+        },
+        '&.Mui-selected': {
+          backgroundColor: alpha(theme.palette.primary.main, 0.8),
+          color: theme.palette.primary.contrastText,
+          '& svg': {
+            color: theme.palette.primary.contrastText,
+          },
+          '&:hover': {
+            backgroundColor: alpha(theme.palette.primary.main, 0.8),
+          },
+        },
+      }),
+    },
+  },
+  MuiListItemText: {
+    styleOverrides: {
+      primary: () => ({
+        fontWeight: 700,
+      }),
+    },
+  },
+  MuiListItemIcon: {
+    styleOverrides: {
+      root: ({ theme }) => ({
+        color: theme.palette.primary.dark,
       }),
     },
   },
