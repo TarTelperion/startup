c4b2b85278a7a5bc6f8a3f9207e48c7010af1e80
Jackson Jones <jacksonpjones05@gmail.com>
2024-04-11 17:43:57 -0600

    fix bug with write

--- src/views/write/Write.jsx
+++ src/views/write/Write.jsx
@@ -57,7 +57,7 @@ const Write = () => {
       <Waiting>
         {story && (
           <>
-            <Flex flexColumn key={Math.random()} id="write-main">
+            <Flex flexColumn key={story._id} id="write-main">
               <EdgyPaper sx={{ width: '100%' }} elevation={4}>
                 <Flex flexColumn p={3} id="edgy-paper-child">
                   <StoryDetails story={story} />
