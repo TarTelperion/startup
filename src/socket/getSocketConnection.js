95128b3a8b561b981c54e85d23a0cd19579491f7
Jackson Jones <jacksonpjones05@gmail.com>
2024-04-12 15:45:08 -0600

    fix poison

--- src/socket/getSocketConnection.js
+++ src/socket/getSocketConnection.js
@@ -5,7 +5,6 @@ const getSocketConnection = async () => {
   if (client) {
     return client
   }
-
   client = io('ws://localhost:4000')
   client.emit('handshake', 'connection established')
   return client
