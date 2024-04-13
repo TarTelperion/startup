b2c2b9a63cdaafb26303ca5272f71c924f649fb5
Jackson Jones <jacksonpjones05@gmail.com>
2024-04-12 20:15:24 -0600

    fix recent edits

--- src/views/home/RecentDashboard.jsx
+++ src/views/home/RecentDashboard.jsx
@@ -1,73 +1,97 @@
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote'
 import { Box, Typography } from '@mui/material'
 import { DateTime } from 'luxon'
+import { useNavigate } from 'react-router-dom'
 import { useMyStories } from '../../hooks/stories/useMyStories'
 import { Flex } from '../../layout'
 import { DashHeader } from './DashHeader'
 
 const RecentDashboard = ({ user }) => {
   const { stories } = useMyStories(user._id)
+  const navigate = useNavigate()
 
-  const findMostRecent = () => {
-    let temp = stories[0]
-    for (let i = 0; i < stories.length; i++) {
-      if (stories[i].updatedAt > temp.updatedAt) {
-        temp = stories[i]
-      }
+  const findMostRecent = (array) => {
+    function sortArray(a, b) {
+      const moveValue = a.updatedAt > b.updatedAt
+      return moveValue ? 1 : -1
     }
-    return temp
+    return array.sort((a, b) => sortArray(a, b))
   }
 
-  const mostRecent = findMostRecent()
+  const mostRecentArray = stories.slice(-2)
+
+  const mostRecentText = (mostRecent) => {
+    let letters = mostRecent.additions[
+      mostRecent.additions.length - 1
+    ].content.slice(0, 150)
+    letters += '...'
+    return letters
+  }
 
   return (
     <Flex flexColumn overflow="hidden">
       <DashHeader title="Recent Activity" />
-      <Box display="flex" flexDirection="row">
-        <Box
-          bgcolor="secondary.main"
-          borderRadius={2}
-          sx={{
-            display: 'flex',
-            padding: 1,
-            alignSelf: 'flex-start',
-            ml: 2,
-            mt: 2,
-          }}
-        >
-          <FormatQuoteIcon
-            sx={{
-              fill: 'white',
-            }}
-          />
-        </Box>
-        {mostRecent && (
-          <Typography ml={2} mt={2}>
-            {mostRecent?.additions?.length === 0
-              ? `${mostRecent.title} was created`
-              : mostRecent?.additions[mostRecent.additions.length - 1]?.content}
-          </Typography>
-        )}
+      <Box maxHeight={300} overflow={'scroll'}>
+        {mostRecentArray.map((mostRecent, index) => {
+          return (
+            <Box key={index}>
+              <Box display="flex" flexDirection="row">
+                <Box
+                  bgcolor="secondary.main"
+                  borderRadius={2}
+                  sx={{
+                    display: 'flex',
+                    padding: 1,
+                    alignSelf: 'flex-start',
+                    ml: 2,
+                    mt: 2,
+                  }}
+                >
+                  <FormatQuoteIcon
+                    sx={{
+                      fill: 'white',
+                    }}
+                    fontSize="large"
+                  />
+                </Box>
+                <Box flexDirection="column">
+                  <Box flexDirection="column" sx={{ mt: 2, ml: 1 }} mb={0}>
+                    <Typography
+                      variant="body"
+                      sx={{
+                        fontWeight: 'bold',
+                        '&:hover': { cursor: 'pointer' },
+                      }}
+                      onClick={() =>
+                        navigate(`/stories/write/${mostRecent._id}`)
+                      }
+                    >
+                      {mostRecent.title}
+                    </Typography>
+                    <Box flexDirection="row" mt={0}>
+                      <Typography variant="caption">
+                        {`${
+                          mostRecent.additions[mostRecent.additions.length - 1]
+                            ?.authorName
+                        }, ${DateTime.fromISO(mostRecent.updatedAt).toRelative()} `}
+                      </Typography>
+                    </Box>
+                  </Box>
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                    ml={2}
+                    mt={2}
+                    sx={{ maxWidth: 'calc(100% - 32px)' }}
+                  >
+                    <Typography>{mostRecentText(mostRecent)}</Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+          )
+        })}
       </Box>
-      {mostRecent && (
-        <Box
-          display="flex"
-          flexDirection="row"
-          ml={2}
-          mt={2}
-          sx={{ maxWidth: 'calc(100% - 32px)' }}
-        >
-          <Typography
-            sx={{
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-            }}
-          >
-            {`--Written ${DateTime.fromISO(mostRecent.updatedAt).toRelative()} by ${mostRecent.additions[mostRecent.additions.length - 1]?.authorName}`}
-          </Typography>
-        </Box>
-      )}
     </Flex>
   )
 }
